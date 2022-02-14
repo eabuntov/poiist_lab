9a8841d3dadf179381bdaf58683fde79aaf9e2bd
Evggenii <e.a.buntov@gmail.com>
2022-02-15 00:21:18 +0500

    first

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import time
+import sys
+import vlc
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def pronounce(sound, start, duration):
+    sound.play()
+    sound.set_time(int(start*1000))
+    time.sleep(duration)
+    sound.stop()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    if len(sys.argv) < 2:
+        print("Too few arguments!")
+    elif sys.argv[1].isdigit():
+        num = int(sys.argv[1])
+        order = len(sys.argv[1])
+        if 0 < num < 1000000000:
+            print("Pronouncing {}".format(num))
+            for n in range(order, 0, -1):
+                digit = num // (10 ** n)
+                num -= digit * 10 ** n
+                if digit > 0:
+                    print(digit)
+                    sound = vlc.MediaPlayer('numbers.mp3')
+                    pronounce(sound, digit*0.7, 0.75)
+            sound.release()
+        else:
+            print("Number out of range!")
+    else:
+        print("Not a number!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
