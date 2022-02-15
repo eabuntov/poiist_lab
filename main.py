76c65b7698482a69d0cad5279ae0929cd1dfa839
eugene <e.a.buntov@gmail.com>
2022-02-15 12:07:19 +0500

    separate numbers

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import time
 import sys
 import vlc
 
-
-def pronounce(sound, start, duration):
+def play_sound(name):
+    sound = vlc.MediaPlayer('sounds/' + name + '.mp3')
     sound.play()
-    sound.set_time(int(start*1000))
-    time.sleep(duration)
+    time.sleep(0.8)
     sound.stop()
+    sound.release()
+
+def pronounce(digit, order):
+    play_sound(str(digit))
+    if order == 6:
+        play_sound('mlns')
+    elif order == 3:
+        play_sound('1000s')
+
 
 
 # Press the green button in the gutter to run the script.
@@ -19,14 +27,13 @@ if __name__ == '__main__':
         order = len(sys.argv[1])
         if 0 < num < 1000000000:
             print("Pronouncing {}".format(num))
-            for n in range(order, 0, -1):
+            for n in range(order, -1, -1):
                 digit = num // (10 ** n)
                 num -= digit * 10 ** n
                 if digit > 0:
                     print(digit)
-                    sound = vlc.MediaPlayer('numbers.mp3')
-                    pronounce(sound, digit*0.7, 0.75)
-            sound.release()
+                    pronounce(digit, n)
+                    #time.sleep(0.5)
         else:
             print("Number out of range!")
     else:
