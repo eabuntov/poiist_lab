6263e2765fb1f6cf602aed0e9e7c338e3b4e63f3
Evggenii <e.a.buntov@gmail.com>
2022-04-17 23:29:22 +0500

    lab4 full pipeline

--- lab4.py
+++ lab4.py
@@ -31,20 +31,6 @@ from skimage.transform import resize
 import numpy as np
 from keras.datasets import mnist
 
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
-
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 print('X_train: ' + str(train_X.shape))
 print('Y_train: ' + str(train_y.shape))
@@ -65,7 +51,7 @@ print('Y_test:  ' + str(test_y.shape))
 # vector classifier on the train samples. The fitted classifier can
 # subsequently be used to predict the value of the digit for the samples
 # in the test subset.
-limit = 100
+limit = 1000
 # flatten the images
 n_samples = len(train_X)
 data = train_X.reshape((n_samples, -1))
@@ -74,18 +60,18 @@ label = test_X.reshape((len(test_X), -1))
 
 knn = KNeighborsClassifier()
 
-k_range = list(range(1, 9))
+k_range = list(range(1, 15))
 param_grid = dict(n_neighbors=k_range)
 
 # defining parameter range
-grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy', return_train_score=False, verbose=1, n_jobs=-1)
+grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy', return_train_score=False, n_jobs=-1)
 
 # fitting the model for grid search
 grid_search = grid.fit(data[0:limit], train_y[0:limit])
 
 print(grid_search.best_params_)
 
-accuracy = grid_search.best_score_ * 100
+accuracy = grid_search.best_score_
 print("Accuracy for our training dataset with tuning is : {:.2f}%".format(accuracy))
 
 clfKNN = KNeighborsClassifier(n_neighbors=grid_search.best_params_['n_neighbors'])
@@ -96,16 +82,14 @@ predicted = clfKNN.predict(label[0:limit])
 # Below we visualize the first 4 test samples and show their predicted
 # digit value in the title.
 
-_, axes = matplotlib.pyplot.subplots(nrows=1, ncols=8, figsize=(10, 3))
+_, axes = matplotlib.pyplot.subplots(nrows=1, ncols=8, figsize=(12, 3))
 for ax, image, prediction in zip(axes, test_X, predicted):
     ax.set_axis_off()
     image = image.reshape(28, 28)
     ax.imshow(image, cmap=matplotlib.pyplot.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
+matplotlib.pyplot.savefig('KNN_result.jpg', bbox_inches='tight')
 
 print(
     f"Classification report for classifier {clfKNN}:\n"
@@ -119,39 +103,21 @@ print(
 disp = metrics.ConfusionMatrixDisplay.from_predictions(test_y[0:limit], predicted)
 disp.figure_.suptitle("Confusion Matrix")
 print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
+matplotlib.pyplot.savefig('KNN_matrix.jpg', bbox_inches='tight')
 matplotlib.pyplot.show()
 
 pipelineSVC = make_pipeline(StandardScaler(), SVC(random_state=1))
-param_grid_svc = [{
+param_grid_svc = {
     'svc__C': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-    'svc__kernel': ['linear']
-},
-    {
-        'svc__C': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__gamma': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__kernel': ['poly']
-    },
-    {
-        'svc__C': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__gamma': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__kernel': ['rbf']
-    },
-    {
-        'svc__C': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__gamma': [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 10.0],
-        'svc__kernel': ['sigmoid']
+    'svc__kernel': ['linear', 'poly', 'rbf', 'sigmoid']
     }
-]
+
 gsSVC = GridSearchCV(estimator=pipelineSVC,
                      param_grid=param_grid_svc,
                      scoring='accuracy',
                      cv=10,
                      refit=True,
                      n_jobs=-1)
-# clf = OneVsRestClassifier(BaggingClassifier(SVC(kernel='linear', probability=True), n_jobs=-1,
-#                                            max_samples=1.0 / 8, n_estimators=8, verbose=True))
-
 
 # Learn the digits on the train subset
 gsSVC.fit(data[0:limit], train_y[0:limit])
@@ -167,13 +133,14 @@ predicted = clfSVC.predict(label[0:limit])
 # Below we visualize the first 4 test samples and show their predicted
 # digit value in the title.
 
-_, axes = matplotlib.pyplot.subplots(nrows=1, ncols=8, figsize=(10, 3))
+_, axes = matplotlib.pyplot.subplots(nrows=1, ncols=8, figsize=(12, 3))
 for ax, image, prediction in zip(axes, test_X, predicted):
     ax.set_axis_off()
     image = image.reshape(28, 28)
     ax.imshow(image, cmap=matplotlib.pyplot.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
 
+matplotlib.pyplot.savefig('SVC_result.jpg', bbox_inches='tight')
 ###############################################################################
 # :func:`~sklearn.metrics.classification_report` builds a text report showing
 # the main classification metrics.
@@ -190,7 +157,7 @@ print(
 disp = metrics.ConfusionMatrixDisplay.from_predictions(test_y[0:limit], predicted)
 disp.figure_.suptitle("Confusion Matrix")
 print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
+matplotlib.pyplot.savefig('SVC_matrix.jpg', bbox_inches='tight')
 matplotlib.pyplot.show()
 
 image = io.imread('digits.jpg')
@@ -200,15 +167,12 @@ segments = np.ndarray((10, 28, 28))
 i = 0
 for section in np.unique(labels):
     rows, cols = np.where(labels == section)
-    print("Image=" + str(section))
-    print("Top-Left pixel = {},{}".format(min(rows), min(cols)))
-    print("Bottom-Right pixel = {},{}".format(max(rows), max(cols)))
     segments[i] = resize(
         crop(image, ((min(rows), image.shape[0] - max(rows)), (min(cols), image.shape[1] - max(cols))), copy=True),
         (28, 28), anti_aliasing=True)
     i = i + 1
-    print("---")
-print(len(segments))
+print('Detected {} segments'.format(len(segments)))
+
 f, axarr = matplotlib.pyplot.subplots(2, 5)
 for i in range(0, 5):
     for j in range(0, 2):
@@ -217,16 +181,18 @@ for i in range(0, 5):
             axarr[j, i].imshow(segments[j * 5 + i], cmap=matplotlib.pyplot.get_cmap('gray'))
         axarr[j, i].axis('off')
 
-matplotlib.pyplot.show()
-data = segments.reshape((10, -1))
+matplotlib.pyplot.savefig('Segmentation_result.jpg', bbox_inches='tight')
+
+
+data = (segments.reshape((10, -1)) < 0.9) * 255
 predicted = clfSVC.predict(data)
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
-
 _, axes = matplotlib.pyplot.subplots(nrows=2, ncols=5, figsize=(12, 3))
-for ax, image, prediction in zip(axes, test_X, predicted):
+for i, image, prediction in zip(range(0, 10), data, predicted):
     image = image.reshape(28, 28)
-    ax.imshow(image, cmap=matplotlib.pyplot.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    axes[i % 2, i // 2].imshow(image, cmap=matplotlib.pyplot.cm.gray_r, interpolation="nearest")
+    axes[i % 2, i // 2].set_title(f"Prediction: {prediction}")
+    axes[i % 2, i // 2].set_axis_off()
+
+matplotlib.pyplot.savefig('Segments_classification.jpg', bbox_inches='tight')
+matplotlib.pyplot.show()
